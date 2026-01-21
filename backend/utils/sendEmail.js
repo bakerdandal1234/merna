4813dc26e49d1b7d2bca7a83d198ba0b61732bcb
bakerdandal1234 <bakerdandal4@gmail.com>
2026-01-21 23:46:15 +0200

    Update sendEmail.js
    da

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,8 +6,8 @@ const sendEmail = async (options) => {
   // إنشاء transporter
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
